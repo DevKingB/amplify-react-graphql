e0dfae691ff493fcf87e9a90d1bc2f67ed8483a7
Brandon Clarke <brandon.clarke1430@gmail.com>
2024-02-11 15:15:24 -0500

    changes for v2

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <h1>Welcome to the Amplify Framework. Hello from V2</h1>
         <a
           className="App-link"
           href="https://reactjs.org"
